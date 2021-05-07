64d286641dd7473d8d5254f6a07f56a926e56a17
AnshGaikwad <anshyg2002@gmail.com>
2021-05-07 12:09:21 +0530

    fixed typos

--- src/vault/modals/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The {@code Steganography.Modals} package contains various <code>JavaFX</code> application modals
- * used by the {@link vault} package to form the application's graphical user interface.
- */
-
-package vault.modals;
--- src/vault/types/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * The {@code Steganography.Types} package contains special data types or <code>enum</code>s used in the {@link vault} package.
- */
-package vault.types;
